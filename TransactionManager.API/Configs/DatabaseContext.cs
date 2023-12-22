a77d53dd2e0725e226ca1631e99903571648d001
Marcin Smolen <smolen.m@e80group.com>
2023-12-22 14:14:18 +0100

    entities and database config

---
+++ TransactionManager.API/Configs/DatabaseContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TransactionManager.API.Entities;
+
+namespace TransactionManager.API.Configs
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options):base(options) { }
+        DbSet<Category> Categories { get; set; }
+        DbSet<User> Users { get; set; }
+        DbSet<Transaction> Transactions { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            CategoriesModelConfigure(modelBuilder);
+            UsersModelConfigure(modelBuilder);
+            TransactionModelConfigure(modelBuilder);
+        }
+        private void CategoriesModelConfigure(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>()
+                .Property(x => x.Name)
+                .IsRequired();
+        }
+        private void UsersModelConfigure(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(x => x.Name)
+                .IsRequired();
+
+            modelBuilder.Entity<User>()
+                .Property(x => x.Password)
+                .IsRequired();
+        }
+        private  void TransactionModelConfigure(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Transaction>()
+                .Property(x => x.Name)
+                .IsRequired();
+
+            modelBuilder.Entity<Transaction>()
+                .Property(x => x.UserId)
+                .IsRequired();
+
+            modelBuilder.Entity<Transaction>()
+                .Property(x => x.Amount)
+                .IsRequired();
+        }
+
+    }
+}
---
+++ TransactionManager.API/Entities/Transaction.cs
@@ -0,0 +1,13 @@
+﻿namespace TransactionManager.API.Entities
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int UserId { get; set; }
+        public double Amount { get; set; }
+        public virtual User User { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
