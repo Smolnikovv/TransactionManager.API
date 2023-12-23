f70d69c1f568cc72423c6c57f3d5af7ba88dcf27
Marcin <thenickram@gmail.com>
2023-12-23 11:25:23 +0100

    basic controllers

---
+++ TransactionManager.API/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TransactionManager.API.Models.Category;
+
+namespace TransactionManager.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly IMediator _mediatR;
+        public CategoryController(IMediator mediator) 
+        {
+            _mediatR = mediator;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById([FromRoute]int id)
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateCategoryDto dto)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ TransactionManager.API/Controllers/TransactionController.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TransactionManager.API.Models.Transaction;
+
+namespace TransactionManager.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class TransactionController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public TransactionController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> GetAllByUserId([FromRoute] int userId)
+        {
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateTransactionDto dto)
+        {
+            return Ok();
+        }
+    }
+}
